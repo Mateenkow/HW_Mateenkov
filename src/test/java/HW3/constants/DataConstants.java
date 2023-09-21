e8635dd41738a9eb32789622132d5013cd5ed7a1
mateenkow <glaymonow@gmail.com>
2023-09-21 23:25:40 +0300

    init commit

---
+++ src/test/java/HW3/constants/DataConstants.java
@@ -0,0 +1,13 @@
+package HW3.constants;
+
+public class DataConstants{
+    public static final String urlForm = "https://app.qa.guru/automation-practice-form/";
+    public static final String nameUser = "Ilya";
+    public static final String lastnameUser = "Mateenkov";
+    public static final String numberUser = "2053454545";
+    public static final String emailUser = "imateenkow@gmail.com";
+    public static final String month = "August";
+    public static final String year = "1998";
+    public static final String subjects = "QA_Guru";
+    public static final String address = "Some Address";
+}
---
+++ src/test/java/SuccessfulFillingFormTest.java
@@ -0,0 +1,66 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import HW3.driver.BaseTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.url;
+import static HW3.constants.DataConstants.*;
+
+public class SuccessfulFillingFormTest extends BaseTest {
+
+    @Test
+    public void successFillFormTest() {
+        open(urlForm);
+        Assertions.assertTrue(url().contains(urlForm));
+
+        $("[placeholder='John']").setValue(nameUser);
+        $("[placeholder='Snow']").setValue(lastnameUser);
+        $("[placeholder='name@example.com']").setValue(emailUser);
+        $("[name='phone']").setValue(numberUser);
+
+        $x("//label[text() = 'Language']/parent::div").click();
+        $("[data-value='Russian']").click();
+
+//        $("[data-testid='CalendarIcon']").click(); Не смогу разобраться
+
+        $("[value='Male']").click();
+        $("[value='Music']").click();
+
+        $x("//label[text() = 'Subjects']/parent::div").click();
+        $("[data-value='Music']").click();
+        $("[data-value='Sports']").click();
+        $("[id='menu-']").click();
+
+        $x("//label[text() = 'State']/parent::div").click();
+        $x("//li[text() = 'Mississippi']").click();
+
+        $x("//label[text() = 'City']/parent::div").click();
+        $x("//li[text() = 'Jackson']").click();
+
+        $(".MuiSlider-thumb").lastChild().setValue("50");
+
+        $("[style='height: 84px; overflow: hidden;']").setValue(address);
+
+//        $("[type='file']").uploadFromClasspath("207.png"); Не смог разобраться
+
+        $("[type='submit']").scrollTo().click();
+
+        $x("//h4[text() = 'Thank you for submitting the form']").scrollTo().shouldBe(Condition.visible);
+        $x("//h4[text() = 'Thank you for submitting the form']/parent::div").shouldBe(text(nameUser));
+        $x("//h4[text() = 'Thank you for submitting the form']/parent::div").shouldBe(text(lastnameUser));
+        $x("//h4[text() = 'Thank you for submitting the form']/parent::div").shouldBe(text(emailUser));
+        $x("//h4[text() = 'Thank you for submitting the form']/parent::div").shouldBe(text("Male"));
+        $x("//h4[text() = 'Thank you for submitting the form']/parent::div").shouldBe(text("+1 205 345 4545"));
+        $x("//h4[text() = 'Thank you for submitting the form']/parent::div").shouldBe(text("Music"));
+        $x("//h4[text() = 'Thank you for submitting the form']/parent::div").shouldBe(text("Music, Sports"));
+        $x("//h4[text() = 'Thank you for submitting the form']/parent::div").shouldBe(text("50"));
+        $x("//h4[text() = 'Thank you for submitting the form']/parent::div").shouldBe(text("Russian"));
+        $x("//h4[text() = 'Thank you for submitting the form']/parent::div").shouldBe(text(address));
+    }
+}
